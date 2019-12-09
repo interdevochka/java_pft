1bc641d7c1b643c118466ad2f7824454bac96516
interdevochka <stitch_girl@mail.ru>
2019-12-09 21:58:07 +0700

    До конца лекций 1.13

--- sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -1,10 +1,13 @@
 package ru.stqa.pft.sandbox;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class SquareTests {
 
-    public void testArea() {
-    //    Square s = new Square(5);
-
-
-    }
+  @Test
+  public void testArea() {
+    Square s = new Square(5);
+    Assert.assertEquals(s.area(), 25.0);
+  }
 }
