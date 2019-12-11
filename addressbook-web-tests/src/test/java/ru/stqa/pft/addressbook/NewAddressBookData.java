3f2e77d63b2ddc4762a36f4e02ac97fc6e6c5f13
interdevochka <stitch_girl@mail.ru>
2019-12-11 23:00:03 +0700

    Задание 4

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/NewAddressBookData.java
@@ -0,0 +1,115 @@
+package ru.stqa.pft.addressbook;
+
+public class NewAddressBookData {
+  private String firstName;
+  private String middleName;
+  private String lastName;
+  private String nickname;
+  private String company;
+  private String address;
+  private String mobile;
+  private String work;
+  private String fax;
+  private String email;
+  private String home;
+  private String title;
+
+  public NewAddressBookData() {
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getMiddleName() {
+    return middleName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getMobile() {
+    return mobile;
+  }
+
+  public String getWork() {
+    return work;
+  }
+
+  public String getFax() {
+    return fax;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getHome() {
+    return home;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public void setMiddleName(String middleName) {
+    this.middleName = middleName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public void setNickname(String nickname) {
+    this.nickname = nickname;
+  }
+
+  public void setCompany(String company) {
+    this.company = company;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public void setMobile(String mobile) {
+    this.mobile = mobile;
+  }
+
+  public void setWork(String work) {
+    this.work = work;
+  }
+
+  public void setFax(String fax) {
+    this.fax = fax;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public void setHome(String home) {
+    this.home = home;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+}
