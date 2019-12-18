6d3a15853855266e1454174da68b7844c789b994
interdevochka <stitch_girl@mail.ru>
2019-12-18 21:43:01 +0700

    Задание 6-попытка 2

--- addressbook-web-tests/src/test/java/tests/EditNewAddressBookEntryTests.java
+++ addressbook-web-tests/src/test/java/tests/EditNewAddressBookEntryTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import model.NewAddressBookData;
 import org.testng.annotations.Test;
 
 public class EditNewAddressBookEntryTests extends TestBase {
@@ -7,8 +8,26 @@ public class EditNewAddressBookEntryTests extends TestBase {
   public void testEditContacts() {
     app.getNavigationHelper().goToHomePage();
     app.getAddressBookHelper().goToEditPage();
-    app.getAddressBookHelper().fillFirstNameField("Petya");
+    app.getAddressBookHelper().fillAddNewAddressBookForm(getNewAddressBookData());
     app.getAddressBookHelper().pressUpdate();
 
+
+  }
+  private NewAddressBookData getNewAddressBookData() {
+    NewAddressBookData contact = new NewAddressBookData();
+    contact.setFirstName("Victor");
+    contact.setMiddleName("Petrovich");
+    contact.setLastName("Galkin");
+    contact.setNickname("Pop");
+    contact.setCompany("Lucoil");
+    contact.setAddress("Novosibirsk, Sovetskaya str. 11-14");
+    contact.setMobile("+791346677");
+    contact.setWork("mywork1");
+    contact.setFax("+79134588");
+    contact.setEmail("test1@mail.ru");
+    contact.setHome("no answer2");
+    contact.setTitle("no title2");
+    return contact;
   }
 }
+
