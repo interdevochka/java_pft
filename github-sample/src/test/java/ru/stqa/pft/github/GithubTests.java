ed477e6ada28b156f7b5910676557790b45b8c92
interdevochka <stitch_girl@mail.ru>
2020-01-27 21:11:05 +0700

    до 20 задания

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,22 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+  @Test
+  public void testCommits() throws IOException {
+    Github github = new RtGithub(" de3238773bf01b921c2f10ac1821124de8034158");
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("interdevochka", "java_pft")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+
+  }
+
+
+}
