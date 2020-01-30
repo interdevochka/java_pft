55bd0c3d26469bb5b3f85a257ab262a494200f5e
interdevochka <stitch_girl@mail.ru>
2020-01-31 00:13:39 +0700

    без capabilities

--- addressbook-web-tests/src/test/java/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/appmanager/ApplicationManager.java
@@ -55,7 +55,7 @@ public class ApplicationManager {
   } else {
       DesiredCapabilities capabilities = new DesiredCapabilities();
       capabilities.setBrowserName(browser);
-      capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win10")));
+      
       wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
 
