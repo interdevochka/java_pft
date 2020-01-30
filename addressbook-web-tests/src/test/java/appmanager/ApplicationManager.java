e3ba8948ec763f945f5f728f131da76cb3f42dd1
interdevochka <stitch_girl@mail.ru>
2020-01-30 17:48:34 +0700

    Реализована возможность удаленного запуска браузера

--- addressbook-web-tests/src/test/java/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/appmanager/ApplicationManager.java
@@ -1,6 +1,7 @@
 package appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.NoAlertPresentException;
 
 import org.openqa.selenium.WebDriver;
@@ -8,10 +9,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -41,12 +45,20 @@ public class ApplicationManager {
 
     dbHelper = new DbHelper();
 
+    if ("".equals(properties.getProperty("selenium.server"))) {
+
+
     if (browser.equals(BrowserType.FIREFOX)) {
       wd = new FirefoxDriver();
     } else if (browser.equals(BrowserType.CHROME)) {
       wd = new ChromeDriver();
     } else if (browser.equals(BrowserType.IE)) {
       wd = new InternetExplorerDriver();
+    }
+  } else {
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setBrowserName(browser);
+      wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
 
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
